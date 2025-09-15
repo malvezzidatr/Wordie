194b29f009937a203cc3e8e4d4fb73b436b5c057
Caio Malvezzi <malvezzi.dev@gmail.com>
2025-09-15 10:46:28 -0300

    init: Started the project

---
+++ src/test/java/br/com/malvezzi/wordie/wordie/WordieApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.malvezzi.wordie.wordie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WordieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
